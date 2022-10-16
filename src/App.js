7ee232225e077bb190a576b3f33f29c18614fbca
Yassine Chettouch <95131504+yassine-ct@users.noreply.github.com>
2022-10-16 13:16:42 +0100

    Calculator hh

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect} from 'react';
+
+
+
+
+function Calculator() {
+
+    // Declaring State for the First Number
+    const [firstValue, setFirstValue] = useState();
+
+    // Declaring State for the Second Number
+    const [secondValue, setSecondValue] = useState();
+
+    const handleFirstChange = (e) => {
+        setFirstValue(e.target.value);
+    }
+
+    const handleSecondChange = (e) => {
+        setSecondValue(e.target.value);
+    }
+
+    return(
+        <>
+            <div className="Calculator">
+                <input type="text" placeholder="First Value" value={firstValue} onChange={handleFirstChange} />
+                <input type="text" placeholder="Second Value" value={secondValue} onChange={handleSecondChange} />
+
+                <div className="Buttons">
+                    <button className="btn btn-default">SUM</button>
+                    <button className="btn btn-default">POWER</button>
+                    <button className="btn btn-default">ROOT</button>
+                </div>
+            </div>
+        </>
+    )
+}
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Calculator/>
     </div>
   );
 }
