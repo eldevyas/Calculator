33c76dda63935f0cc9bd466df38ce7696e583c3f
Yassine Chettouch <95131504+yassine-ct@users.noreply.github.com>
2022-10-16 14:10:34 +0100

    Calculator Complete 📰📦

--- src/App.js
+++ src/App.js
@@ -1,16 +1,66 @@
 import './App.css';
-import React, { useState, useEffect} from 'react';
+import React, { useState } from 'react';
+
+function SumButton(props) {
+    let firstValue = props.firstValue;
+    let secondValue = props.secondValue;
+    const setResult = props.setResult;
+
+    const handleSum = () => {
+        const finalResult = Number(firstValue) + Number(secondValue);
+        setResult(finalResult);
+    }
+
+    return <button onClick={handleSum}> <span>Sum</span></button>;
+}
+
+function PowerButton(props) {
+    let firstValue = props.firstValue;
+    let secondValue = props.secondValue;
+    const setResult = props.setResult;
+
+    const handlePower = () => {
+        const finalResult = Number(firstValue) * Number(secondValue);
+        setResult(finalResult);
+    }
+
+    return <button onClick={handlePower}> <span>Power</span></button>;
+}
 
 
+function RootButton(props) {
+    let firstValue = props.firstValue;
+    let secondValue = props.secondValue;
+    const setResult = props.setResult;
 
+    const handleRoot = () => {
+        // Hadchi fih lmath rbek
+        let A = Number(firstValue);
+        let B = Number(secondValue);
+
+        function calcHypotenuse(a, b) {
+            return (Math.sqrt((a * a) + (b * b)));
+        }
+
+        var Formula = calcHypotenuse(A, B);
+
+        setResult(Formula);
+    }
+
+    return <button onClick={handleRoot}> <span>Root</span></button>;
+
+}
 
 function Calculator() {
-
     // Declaring State for the First Number
-    const [firstValue, setFirstValue] = useState();
+    const [firstValue, setFirstValue] = useState(0);
 
     // Declaring State for the Second Number
-    const [secondValue, setSecondValue] = useState();
+    const [secondValue, setSecondValue] = useState(0);
+
+    // Declaring State for the Third Number (Result)
+    const [Result, setResult] = useState(0);
+
 
     const handleFirstChange = (e) => {
         setFirstValue(e.target.value);
@@ -23,14 +73,16 @@ function Calculator() {
     return(
         <>
             <div className="Calculator">
-                <input type="text" placeholder="First Value" value={firstValue} onChange={handleFirstChange} />
-                <input type="text" placeholder="Second Value" value={secondValue} onChange={handleSecondChange} />
+                <input type="number" placeholder="First Value" value={firstValue} onChange={handleFirstChange} />
+                <input type="number" placeholder="Second Value" value={secondValue} onChange={handleSecondChange} />
 
                 <div className="Buttons">
-                    <button className="btn btn-default">SUM</button>
-                    <button className="btn btn-default">POWER</button>
-                    <button className="btn btn-default">ROOT</button>
+                    <SumButton firstValue={firstValue} secondValue={secondValue} setResult={setResult}/>
+                    <PowerButton firstValue={firstValue} secondValue={secondValue} setResult={setResult}/>
+                    <RootButton firstValue={firstValue} secondValue={secondValue} setResult={setResult}/>
                 </div>
+
+                <input type="number" disabled placeholder="Result" value={Result}/>
             </div>
         </>
     )
